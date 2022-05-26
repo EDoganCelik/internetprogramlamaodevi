4ec813f67390d140297475d7d1b17ec15d07229c
Emre Dogan Celik <officalemredogan@gmail.com>
2022-05-27 00:17:11 +0300

    hata düzeltme
    düzeltme

--- npm-test/router/error_router.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const path = require("path");
-
-router.get("*", function (req, res) {
-  res.render("error", { error: "Portal silahınız kırılmış olmalı" });
-});
-module.exports = router;
--- npm-test/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose
-  .connect(process.env.MONGODB_CONNECTION_STRING_LOCAL, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .then(() => console.log("veritabanına bağlanıldı"))
-  .catch((hata) => console.log(`veritabanı baglantı hatası ${hata}`));
